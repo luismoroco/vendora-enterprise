bb890da6c4e04eee52c14eaf5ae35930c7edbde6
lmoroco <lmorocoramos@gmail.com>
2026-01-11 20:25:41 -0500

    chore: init backend module

---
+++ module/vendora-backend/src/test/java/com/vendora/backend/VendoraBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vendora.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendoraBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
